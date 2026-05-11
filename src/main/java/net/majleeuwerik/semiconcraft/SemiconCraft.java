7f8360d615f0c70aaf8182dee6080d6e1cf04f2b
Marinus Leeuwerik <mail@majleeuwerik.net>
2026-05-11 08:57:45 +0200

    Fixed block/item registration. Broke textures and something with translation I think

--- src/main/java/net/majleeuwerik/semiconcraft/SemiconCraft.java
+++ src/main/java/net/majleeuwerik/semiconcraft/SemiconCraft.java
@@ -1,5 +1,8 @@
 package net.majleeuwerik.semiconcraft;
 
+import net.majleeuwerik.semiconcraft.content.item.SCCreativeModeTab;
+import net.majleeuwerik.semiconcraft.registry.SCBlocks;
+import net.majleeuwerik.semiconcraft.registry.SCItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -38,30 +41,6 @@ public class SemiconCraft {
     public static final String MODID = "semiconcraft";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "semiconcraft" namespace
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "semiconcraft" namespace
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "semiconcraft" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
-
-    // Creates a new Block with the id "semiconcraft:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", p -> p.mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "semiconcraft:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
-
-    // Creates a new food item with the id "semiconcraft:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", p -> p.food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
-
-    // Creates a creative tab with the id "semiconcraft:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.semiconcraft")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
@@ -69,12 +48,6 @@ public class SemiconCraft {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
-        // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so items get registered
-        ITEMS.register(modEventBus);
-        // Register the Deferred Register to the mod event bus so tabs get registered
-        CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (SemiconCraft) to respond directly to events.
@@ -83,6 +56,13 @@ public class SemiconCraft {
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
+
+        SCCreativeModeTab.register(modEventBus);
+
+        SCItems.register(modEventBus);
+        SCBlocks.register(modEventBus);
+
+
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
@@ -103,8 +83,13 @@ public class SemiconCraft {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(SCItems.SILICON_BOULE);
+            //event.accept(SCItems.SILICON_CRYSTAL);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            //event.accept(SCBlocks.CRYSTAL_ORE);
+            event.accept(SCBlocks.CRYSTAL_BLOCK);
         }
     }
 
